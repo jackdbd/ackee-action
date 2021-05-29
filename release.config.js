81172b450cf5acd5d4cb84d906989e79bc85638a
Giacomo Debidda <jackdebidda@gmail.com>
2021-05-29 02:08:34 +0200

    ci: add npm plugin to bump version in package.json

--- release.config.js
+++ release.config.js
@@ -4,6 +4,14 @@
 // That's why there is this: ../../commitlint.config.js
 // Note: each plugin is configured as an array. It's a bit weird...
 
+// https://github.com/semantic-release/changelog
+const changelog = [
+  '@semantic-release/changelog',
+  {
+    changelogFile: 'CHANGELOG.md'
+  }
+]
+
 // https://github.com/semantic-release/commit-analyzer
 // I prefer to keep the configuration for the commit linter in
 // commitlint.config.js,so I can run npm run lint even when I am not releasing
@@ -16,6 +24,21 @@ const commitAnalyzer = [
   '@semantic-release/commit-analyzer',
   {
     config: './commitlint.config.js'
+  }
+]
+
+// https://github.com/semantic-release/git
+const git = [
+  '@semantic-release/git',
+  {
+    assets: [
+      'dist/index.js',
+      'CHANGELOG.md',
+      'package.json',
+      'package-lock.json'
+    ],
+    message:
+      'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
   }
 ]
 
@@ -35,20 +58,9 @@ const github = [
   }
 ]
 
-// https://github.com/semantic-release/git
-const git = [
-  '@semantic-release/git',
-  {
-    assets: [
-      'dist/index.js',
-      'CHANGELOG.md',
-      'package.json',
-      'package-lock.json'
-    ],
-    message:
-      'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-  }
-]
+// https://github.com/semantic-release/npm
+// Even if not publishing on npm, the npm plugin is needed to bump the version.
+const npm = ['@semantic-release/npm', { npmPublish: false, pkgRoot: '.' }]
 
 // https://github.com/semantic-release/release-notes-generator
 const releaseNotesGenerator = [
@@ -58,22 +70,15 @@ const releaseNotesGenerator = [
   }
 ]
 
-// https://github.com/semantic-release/changelog
-const changelog = [
-  '@semantic-release/changelog',
-  {
-    changelogFile: 'CHANGELOG.md'
-  }
-]
-
 const config = {
   // https://semantic-release.gitbook.io/semantic-release/usage/configuration#branches
   branches: ['main', 'release'],
   ci: true,
-  debug: true,
-  plugins: [commitAnalyzer, releaseNotesGenerator, changelog, github, git]
+  // The git plugin must be called after the npm plugin. See here:
+  // https://github.com/semantic-release/git#examples
+  plugins: [commitAnalyzer, releaseNotesGenerator, changelog, npm, github, git]
 }
 
-console.log('=== semantic-release config ===', config)
+// console.log('=== semantic-release config ===', config)
 
 module.exports = config
